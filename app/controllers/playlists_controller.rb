1692bdfac74bd760d0c722a65ce9337f111c98c4
Nicolas Delage <ndelage90@gmail.com>
2019-06-19 15:26:34 +0200

    can now add playlist to DB and display them

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,37 +1,39 @@
 class PlaylistsController < ApplicationController
 
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :new]
   require "json"
   require "rest-client"
 
   def index
-    id1 = '0NEJ0N8MDVOwMWVmLFO0Jj'
+    @spotifyPlaylists = []
+    playlists = Playlist.all
 
-    myClientKey = ENV["CLIENT_KEY"]
-
-    tokenResponse = RestClient.post("https://accounts.spotify.com/api/token",
-      {'grant_type' => 'client_credentials'},
-      {'Content-Type' => 'application/x-www-form-urlencoded', 'Authorization' => myClientKey})
-
-    token = JSON.parse(tokenResponse)
-
-    mykey = "Bearer " + token["access_token"]
-
-    response1 = RestClient.get("https://api.spotify.com/v1/playlists/" + id1,{'Authorization' => mykey})
-    @playlist1 = JSON.parse(response1)
-
-    id2 = '54ZyhTBvjoJpVBpIgb8t7K'
-    response2 = RestClient.get("https://api.spotify.com/v1/playlists/" + id2,{'Authorization' => mykey})
-    @playlist2 = JSON.parse(response2)
+    playlists.each do |p|
+      @spotifyPlaylists << get_playlist(p.SpotifyId)
+    end
   end
 
   def show
   end
 
   def new
+    @playlist = Playlist.new
   end
 
   def create
+    @playlist = Playlist.new(playlist_params)
+    spotifyPlaylist = get_playlist(params[:playlist][:SpotifyId])
+    @playlist.description = spotifyPlaylist['description']
+    @playlist.name = spotifyPlaylist['name']
+    @playlist.ownerName = spotifyPlaylist['owner']['display_name']
+    @playlist.ownerId = spotifyPlaylist['owner']['id']
+
+    if @playlist.save
+      redirect_to playlists_path
+    else
+      render :new
+    end
+
   end
 
   def update
@@ -47,5 +49,20 @@ class PlaylistsController < ApplicationController
 
   def find_playlist
     @playlist = Playlist.find(params[:id])
+  end
+
+  def playlist_params
+    params.require(:playlist).permit(:SpotifyId) # , places_attributes: [:places, :name, :address])
+  end
+
+  def get_playlist(id)
+    myClientKey = ENV["CLIENT_KEY"]
+    tokenResponse = RestClient.post("https://accounts.spotify.com/api/token",
+      {'grant_type' => 'client_credentials'},
+      {'Content-Type' => 'application/x-www-form-urlencoded', 'Authorization' => myClientKey})
+    token = JSON.parse(tokenResponse)
+    mykey = "Bearer " + token["access_token"]
+    response1 = RestClient.get("https://api.spotify.com/v1/playlists/" + id,{'Authorization' => mykey})
+    return spotifyPlaylist = JSON.parse(response1)
   end
 end
