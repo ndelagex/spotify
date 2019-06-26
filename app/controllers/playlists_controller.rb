29a8f03688b20e51d6328b20f56c7cdb1dca21ff
Nicolas Delage <ndelage90@gmail.com>
2019-06-26 22:25:57 +0200

    set spotify helper and computed average

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,5 +1,7 @@
 class PlaylistsController < ApplicationController
 
+  include ::SpotifyHelper
+
   skip_before_action :authenticate_user!, only: [:index, :new]
   require "json"
   require "rest-client"
@@ -55,14 +57,4 @@ class PlaylistsController < ApplicationController
     params.require(:playlist).permit(:SpotifyId) # , places_attributes: [:places, :name, :address])
   end
 
-  def get_playlist(id)
-    myClientKey = ENV["CLIENT_KEY"]
-    tokenResponse = RestClient.post("https://accounts.spotify.com/api/token",
-      {'grant_type' => 'client_credentials'},
-      {'Content-Type' => 'application/x-www-form-urlencoded', 'Authorization' => myClientKey})
-    token = JSON.parse(tokenResponse)
-    mykey = "Bearer " + token["access_token"]
-    response1 = RestClient.get("https://api.spotify.com/v1/playlists/" + id,{'Authorization' => mykey})
-    return spotifyPlaylist = JSON.parse(response1)
-  end
 end
